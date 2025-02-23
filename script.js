20c7742d0b02e96013b52279dc8c30da6afa061f
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-23 10:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
     //Write your code here
+    let fname_val = document.getElementsByName('fname');
+    let lname_val = document.getElementsByName('lname');
 
+    for (let i = 0; i < fname_val.length; i++) {
+        console.log(fname_val[i].value);
+    }
+    for (let j = 0; j < lname_val.length; j++) {
+        console.log(lname_val[j].value);
+    }
+
+    alert(`${fname_val[0].value} ${lname_val[0].value}.`);
 }
+let btn_val = document.getElementById('submit');
+btn_val.addEventListener('click', function (event) {
+    event.preventDefault();
+    getFormvalue();
+});
