0a88aea2fddadb65a5c84d69c98aea1abefcac84
Gulamnabi Kagathala <145833927+gulu43@users.noreply.github.com>
2025-03-09 05:42:17 -0300

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-function getFormvalue() {
-    //Write your code here
-    let fname_val = document.getElementsByName('fname');
-    let lname_val = document.getElementsByName('lname');
+document.getElementById("myForm").addEventListener("submit", getFormValue);
 
-    for (let i = 0; i < fname_val.length; i++) {
-        console.log(fname_val[i].value);
-    }
-    for (let j = 0; j < lname_val.length; j++) {
-        console.log(lname_val[j].value);
-    }
-
-    alert(`${fname_val[0].value} ${lname_val[0].value}.`);
+function getFormValue(e) {
+    e.preventDefault();
+    const fname = document.getElementById("fname").value.trim();
+    const lname = document.getElementById("lname").value.trim();
+    const fullName = `${fname} ${lname}`;
+    alert(fullName);
 }
-let btn_val = document.getElementById('submit');
-btn_val.addEventListener('click', function (event) {
-    event.preventDefault();
-    getFormvalue();
-});
